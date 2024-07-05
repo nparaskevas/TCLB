b09f11b5c0b87d81ec21e1b8e0874baaa523f159
nparaskevas <n.paraskevas@uqconnect.edu.au>
2024-07-05 12:52:38 +1000

    Changed the normal grad fields name for clarity

--- models/multiphase/d3q27_pf_velocity/Dynamics.R
+++ models/multiphase/d3q27_pf_velocity/Dynamics.R
@@ -1,8 +1,3 @@
-#	TODO: Change the normalised gradphi term from simply being gradPhiVal_x etc. to nGradPhiVal_x etc.,
-#	It is misleading in its current form, and can cause compatibility issues with the geometric boundary 
-#	condition. This needs to be done in both the Dynamics.c.Rt and the Dynamics.R file.
-#
-
 # Setting permissive access policy.
 #  * This skips checks of fields being overwritten or read prematurely.
 #  * Otherwise the model compilation was failing.
@@ -119,9 +114,9 @@ if (Options$geometric){
 
     AddField("PhaseF",stencil3d=2, group="PF")
 } else if (Options$SurfStress) {
-	AddField("gradPhiVal_x", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
-	AddField("gradPhiVal_y", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
-	AddField("gradPhiVal_z", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
+	AddField("nGradPhiVal_x", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
+	AddField("nGradPhiVal_y", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
+	AddField("nGradPhiVal_z", stencil3d=1, group="gradPhi", optimise_for_static_access = FALSE)
 	AddField("PhaseF",stencil3d=1, group="PF")
 
 } else {
